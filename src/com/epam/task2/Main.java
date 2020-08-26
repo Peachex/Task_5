6840dc9e4ee2e39379a5f4265c47d64860cab619
Peachex <klevolex@gmail.com>
2020-08-26 12:02:52 +0300

    fix task2

--- src/com/epam/task2/Main.java
+++ src/com/epam/task2/Main.java
@@ -6,32 +6,31 @@ import java.util.Scanner;
    Создать класс Payment с внутренним классом, с помощью объектов которого можно сформировать покупку из
    нескольких товаров. */
 public class Main {
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        Payment payment = new Payment();
-        while (true) {
-            System.out.println("1)Choose product.\n2)Cart.\n3)Buy.\n4)Exit.\n");
-            switch (in.nextInt()) {
-                case 1: {
-                    payment.chooseProducts();
-                    break;
-                }
-                case 2: {
-                    payment.viewCart();
-                    break;
-                }
-                case 3: {
-                    payment.buyProducts();
-                    break;
-                }
-                case 4: {
-                    return;
-                }
-                default: {
-                    System.out.println("Something wrong...\n");
-                }
-            }
-        }
-    }
+	public static void main(String[] args) {
+		Payment payment = new Payment();
+		while (true) {
+			System.out.println("1)Choose product.\n2)Cart.\n3)Buy.\n4)Exit.\n");
+			switch (new Scanner(System.in).nextInt()) {
+				case 1: {
+					payment.chooseProducts();
+					break;
+				}
+				case 2: {
+					payment.viewCart();
+					break;
+				}
+				case 3: {
+					payment.buyProducts();
+					break;
+				}
+				case 4: {
+					return;
+				}
+				default: {
+					System.out.println("\nSomething wrong...\n");
+				}
+			}
+		}
+	}
 }
 
--- src/com/epam/task2/Payment.java
+++ src/com/epam/task2/Payment.java
@@ -1,117 +1,79 @@
 package com.epam.task2;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Payment {
-    private static int index = 0;
-    private Products[] cart = new Products[Products.values().length];
-    public Payment() {
+	public enum Product {
+		BREAD(1.99), MILK(2.49), CHEESE(1.69), ICE_CREAM(3.99), CAKE(7.0), WATER(0.99), MEAT(19.29), CHICKEN(16.99);
 
-    }
+		private double price;
 
-    public static int getIndex() {
-        return index;
-    }
+		Product(double price) {
+			this.price = price;
+		}
 
-    public static void setIndex(int index) {
-        Payment.index = index;
-    }
+		public double getPrice() {
+			return this.price;
+		}
 
-    public Products[] getCart() {
-        return this.cart;
-    }
+		public void setPrice(double price) {
+			this.price = price;
+		}
 
-    public void setCart(Products[] cart) {
-        this.cart = cart;
-    }
+		private static void viewProducts() {
+			System.out.println();
+			for (Product product : Product.values()) {
+				System.out.println((product.ordinal() + 1) + ")" + product.name() + " - " + product.price + "$");
+			}
+		}
+	}
 
-    public void chooseProducts() {
-        if (index == Products.values().length) {
-            System.out.println("\nYour cart is full!\n");
-            return;
-        }
-        Products.viewProducts();
-        Scanner in = new Scanner(System.in);
-        System.out.print("\nChoose product: ");
-        int toggle = in.nextInt() - 1;
-        if (toggle >= 0 && toggle < Products.values().length) {
-            this.cart[index++] = Products.values()[toggle];
-            System.out.println("Added in your cart.\n");
-        } else {
-            System.out.println("Something wrong...\n");
-        }
-    }
+	private List<Product> cart = new ArrayList<>();
 
-    public double totalCost() {
-        double totalCost = 0;
-        for (Products products : this.cart) {
-            if (products != null) {
-                totalCost += products.cost();
-            } else {
-                break;
-            }
-        }
-        return totalCost;
-    }
+	public Payment() {
 
-    public void viewCart() {
-        System.out.println("\nYour cart:");
-        for (Products products : this.cart) {
-            if (products != null) {
-                System.out.println(products.name() + " - " + products.cost() + "$");
-            } else {
-                break;
-            }
-        }
-        System.out.println("----------------------\nTotal cost = " + this.totalCost() + "\n");
-    }
+	}
 
-    public void buyProducts() {
-        System.out.println("\nPay " + this.totalCost() + "$\n");
-        this.cart = new Products[Products.values().length];
-        index = 0;
-    }
+	public List<Product> getCart() {
+		return this.cart;
+	}
 
-    public enum Products {
-        BREAD, MILK, CHEESE, ICE_CREAM, CAKE, WATER, MEAT, CHICKEN;
+	public void addToCart(Product product) {
+		this.cart.add(product);
+	}
 
-        private static void viewProducts() {
-            for (Products pr : Products.values()) {
-                System.out.println((pr.ordinal() + 1) + ")" + pr.name() + " - " + pr.cost() + "$");
-            }
-        }
+	public void chooseProducts() {
+		Product.viewProducts();
+		System.out.print("\nChoose product: ");
+		try {
+			Product product = Product.valueOf(new Scanner(System.in).next().toUpperCase());
+			cart.add(product);
+			System.out.println("\nAdded in your cart.\n");
+		} catch (IllegalArgumentException e) {
+			System.out.println("\nSomething wrong...\n");
+		}
+	}
 
-        private double cost() {
-            switch (this) {
-                case BREAD: {
-                    return 1.99;
-                }
-                case MILK: {
-                    return 2.49;
-                }
-                case CHEESE: {
-                    return 1.69;
-                }
-                case ICE_CREAM: {
-                    return 3.99;
-                }
-                case CAKE: {
-                    return 7.0;
-                }
-                case WATER: {
-                    return 0.99;
-                }
-                case MEAT: {
-                    return 19.29;
-                }
-                case CHICKEN: {
-                    return 16.99;
-                }
-                default: {
-                    System.out.println("Unknown product!");
-                }
-            }
-            return 0;
-        }
-    }
+	public double totalCost() {
+		double totalCost = 0;
+		for (Product product : this.cart) {
+			totalCost += product.price;
+		}
+		return totalCost;
+	}
+
+	public void viewCart() {
+		System.out.println("\nYour cart:");
+		for (Product product : this.cart) {
+			System.out.println(product.name() + " - " + product.price + "$");
+		}
+		System.out.println("----------------------\nTotal cost = " + this.totalCost() + "\n");
+	}
+
+	public void buyProducts() {
+		System.out.println("\nPay " + this.totalCost() + "$\n");
+		this.cart = new ArrayList<>();
+	}
 }
