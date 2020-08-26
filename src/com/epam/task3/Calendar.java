5cc30fd3bad360d53ca67a29af2fb53cd2dfd63b
Peachex <klevolex@gmail.com>
2020-08-26 12:11:57 +0300

    fix task3

--- src/com/epam/task3/Calendar.java
+++ src/com/epam/task3/Calendar.java
@@ -1,88 +1,69 @@
 package com.epam.task3;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 
 public class Calendar {
-    public class Note {
-        private LocalDate date;
-        private String noteText;
+	public static class Note {
+		private LocalDate date;
+		private String noteText;
 
-        public Note(LocalDate date, String noteText) {
-            this.date = date;
-            this.noteText = noteText;
-        }
+		public Note(LocalDate date, String noteText) {
+			this.date = date;
+			this.noteText = noteText;
+		}
 
-        public LocalDate getDate() {
-            return this.date;
-        }
+		public LocalDate getDate() {
+			return this.date;
+		}
 
-        public void setDate(LocalDate date) {
-            this.date = date;
-        }
+		public void setDate(LocalDate date) {
+			this.date = date;
+		}
 
-        public String getNoteText() {
-            return this.noteText;
-        }
+		public String getNoteText() {
+			return this.noteText;
+		}
 
-        public void setNoteText(String noteText) {
-            this.noteText = noteText;
-        }
+		public void setNoteText(String noteText) {
+			this.noteText = noteText;
+		}
 
-        public String toString() {
-            return this.date.toString() + " - " + this.noteText;
-        }
-    }
+		public String toString() {
+			return this.date.toString() + " - " + this.noteText;
+		}
+	}
 
-    private static final int size = 100;
-    private Note[] notes = new Note[size];
-    private int index = 0;
+	private ArrayList<Note> notes = new ArrayList<>();
 
-    public Calendar() {
+	public Calendar() {
 
-    }
+	}
 
-    public Note[] getNotes() {
-        return this.notes;
-    }
+	public ArrayList<Note> getNotes() {
+		return this.notes;
+	}
 
-    public void setNotes(Note[] notes) {
-        this.notes = notes;
-    }
+	public void addToNotes(Note note) {
+		this.notes.add(note);
+	}
 
-    public int getIndex() {
-        return this.index;
-    }
+	public void createNote(Note note) {
+		this.notes.add(note);
+	}
 
-    public void setIndex(int index) {
-        this.index = index;
-    }
+	public void viewNotes() {
+		System.out.print(this);
+	}
 
-    public static int getSize() {
-        return size;
-    }
-
-    public void createNote(Note... notes) {
-        if (this.index < size) {
-            for (Note note : notes) {
-                this.notes[index++] = note;
-            }
-        } else {
-            System.out.println("\nSomething wrong...\n");
-        }
-    }
-
-    public void viewNotes() {
-        System.out.print(this);
-    }
-
-    public String toString() {
-        for (Note note : this.notes) {
-            if (note != null) {
-                System.out.println(note);
-            } else {
-                break;
-            }
-        }
-        return "";
-    }
+	public String toString() {
+		for (Note note : this.notes) {
+			if (note != null) {
+				System.out.println(note);
+			} else {
+				break;
+			}
+		}
+		return "";
+	}
 }
--- src/com/epam/task3/Main.java
+++ src/com/epam/task3/Main.java
@@ -6,18 +6,17 @@ import java.time.LocalDate;
    Создать класс Календарь с внутренним классом, с помощью объектов которого можно хранить информацию о выходных и
    праздничных днях. */
 public class Main {
-    public static void main(String[] args) {
-        LocalDate date1 = LocalDate.of(2020, 11, 11);
-        LocalDate date2 = LocalDate.of(2020, 12, 31);
-        LocalDate date3 = LocalDate.of(2020, 4, 1);
+	public static void main(String[] args) {
+		LocalDate date1 = LocalDate.of(2020, 11, 11);
+		LocalDate date2 = LocalDate.of(2020, 12, 31);
+		LocalDate date3 = LocalDate.of(2020, 4, 1);
 
-        Calendar calendar = new Calendar();
+		Calendar calendar = new Calendar();
 
-        Calendar.Note note1 = calendar.new Note(date1, "My birthday.");
-        Calendar.Note note2 = calendar.new Note(date2, "New Year.");
-        Calendar.Note note3 = calendar.new Note(date3, "Fool's day.");
+		calendar.createNote(new Calendar.Note(date1, "My birthday."));
+		calendar.createNote(new Calendar.Note(date2, "New Year's Eve."));
+		calendar.createNote(new Calendar.Note(date3, "Fool's day."));
 
-        calendar.createNote(note1, note2, note3);
-        calendar.viewNotes();
-    }
+		calendar.viewNotes();
+	}
 }
