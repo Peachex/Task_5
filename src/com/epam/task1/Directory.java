897d04b94b93fd8880dcbcfa987f8923a29361e8
Peachex <klevolex@gmail.com>
2020-08-26 11:41:55 +0300

    fix task1

---
+++ src/com/epam/task1/Directory.java
@@ -0,0 +1,17 @@
+package com.epam.task1;
+
+public class Directory {
+	private String directoryPath;
+
+	public Directory(String directoryPath) {
+		this.directoryPath = directoryPath;
+	}
+
+	public String getDirectoryPath() {
+		return this.directoryPath;
+	}
+
+	public void setDirectoryPath(String directoryPath) {
+		this.directoryPath = directoryPath;
+	}
+}
--- src/com/epam/task1/TextFile.java
+++ src/com/epam/task1/TextFile.java
@@ -1,92 +1,91 @@
 package com.epam.task1;
 
-import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.nio.file.Path;
 import java.nio.file.Files;
+import java.nio.file.Paths;
 
-public class TextFile {
-    private Path path;
+public class TextFile extends File {
+	public TextFile(File file) {
+		super(file.getDirectoryPath(), file.getFileName());
+	}
 
-    public TextFile(Path path) {
-        this.path = path;
-    }
+	public boolean createFile() {
+		if (!Files.exists(this.getPath())) {
+			try {
+				Files.createFile(this.getPath());
+				return true;
+			} catch (IOException e) {
+				System.out.println("\nCouldn't create!\n" + e.getMessage());
+			}
+		} else {
+			System.out.println("\nFile already exists!");
+		}
+		return false;
+	}
 
-    public Path getPath() {
-        return this.path;
-    }
+	public boolean deleteFile() {
+		if (Files.exists(this.getPath())) {
+			try {
+				Files.delete(this.getPath());
+				return true;
+			} catch (IOException e) {
+				System.out.println("\nCouldn't delete!\n" + e.getMessage());
+			}
+		} else {
+			System.out.println("File already exists!");
+		}
+		return false;
+	}
 
-    public void setPath(Path path) {
-        this.path = path;
-    }
+	public boolean renameFile(String path, String fileName) {
+		if (Files.exists(this.getPath())) {
+			try {
+				Files.move(this.getPath(), Paths.get(path.concat(java.io.File.separator).concat(fileName)));
+				this.setDirectoryPath(path);
+				this.setFileName(fileName);
+				return true;
+			} catch (IOException e) {
+				System.out.println(e.getMessage());
+			}
+		} else {
+			System.out.println("File already exists!");
+		}
+		return false;
+	}
 
-    public boolean createFile() {
-        if (!Files.exists(this.path)) {
-            try {
-                Files.createFile(this.path);
-                return true;
-            } catch (IOException e) {
-                System.out.println("\nCouldn't create!\n" + e.getMessage());
-            }
-        }
-        return false;
-    }
+	public boolean write(String str) {
+		if (Files.exists(this.getPath())) {
+			try {
+				FileOutputStream outFile = new FileOutputStream(this.getPath().toString(), true);
+				outFile.write(str.getBytes());
+				outFile.close();
+				return true;
+			} catch (IOException e) {
+				System.out.println("\nCouldn't write!\n" + e.getMessage());
+			}
+		} else {
+			System.out.println("File already exists!");
+		}
+		return false;
+	}
 
-    public boolean deleteFile() {
-        if (Files.exists(this.path)) {
-            try {
-                Files.delete(this.path);
-                return true;
-            } catch (IOException e) {
-                System.out.println("\nCouldn't delete!\n" + e.getMessage());
-            }
-        }
-        return false;
-    }
-
-    public boolean renameFile(Path newPath) {
-        if (Files.exists(this.path)) {
-            try {
-                Files.move(this.path, newPath);
-                this.path = newPath;
-                return true;
-            } catch (IOException e) {
-                System.out.println(e.getMessage());
-            }
-        }
-        return false;
-    }
-
-    public boolean write(String str) {
-        if (Files.exists(this.path)) {
-            try {
-                FileOutputStream outFile = new FileOutputStream(this.path.toString(), true);
-                outFile.write(str.getBytes());
-                outFile.close();
-                return true;
-            } catch (IOException e) {
-                System.out.println("\nCouldn't write!\n" + e.getMessage());
-            }
-        }
-        return false;
-    }
-
-    public boolean read() {
-        if (Files.exists(this.path)) {
-            try {
-                FileInputStream inFile = new FileInputStream(this.path.toString());
-                while (inFile.available() != 0) {
-                    System.out.print((char) inFile.read());
-                }
-                inFile.close();
-                return true;
-            } catch (IOException e) {
-                System.out.println("\nCouldn't read!\n" + e.getMessage());
-            }
-        }
-        return false;
-    }
-
+	public boolean read() {
+		if (Files.exists(this.getPath())) {
+			try {
+				FileInputStream inFile = new FileInputStream(this.getPath().toString());
+				while (inFile.available() != 0) {
+					System.out.print((char) inFile.read());
+				}
+				inFile.close();
+				return true;
+			} catch (IOException e) {
+				System.out.println("\nCouldn't read!\n" + e.getMessage());
+			}
+		} else {
+			System.out.println("File already exists!");
+		}
+		return false;
+	}
 }
